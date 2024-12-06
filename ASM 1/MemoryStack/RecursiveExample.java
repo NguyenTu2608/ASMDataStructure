0b6ebeede28a0ac6ffa1f9e0ecde72db55a7664c
NguyenTu2608 <atuisme2608@gmail.com>
2024-12-06 19:34:59 +0700

    update
    update

---
+++ ASM 1/MemoryStack/RecursiveExample.java
@@ -0,0 +1,15 @@
+package MemoryStack;
+
+public class RecursiveExample {
+    public static void main(String[] args) {
+        int num = 5;
+        int result = factorial(num);
+        System.out.println("Factorial of " + num + " is " + result);
+    }
+    public static int factorial(int n) {
+        if (n == 0) return 1;
+        return n * factorial(n - 1);
+    }
+}
+
+
